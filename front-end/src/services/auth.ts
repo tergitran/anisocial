9720eed0dee06e8477b5fec4b40a17f2b014c395
Tran Dan <tergitran@gmail.com>
2023-05-09 17:15:24 +0700

    show log env

--- front-end/src/services/auth.ts
+++ front-end/src/services/auth.ts
@@ -1,10 +1,11 @@
 const baseUrl = import.meta.env.VITE_BASE_URL_AUTH ?? 'http://localhost:5500/api/v1';
+const env = import.meta.env;
 
 import axios from "@/axios";
 
 export default {
   login(username: string, password: string) {
-    console.log('import.meta.env', import.meta.env);
+    console.log('env', env);
     console.log('baseUrl', baseUrl);
     
     return axios.post(baseUrl + '/login', { username, password });
